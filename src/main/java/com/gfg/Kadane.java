2182dca3b0732f1bbc53b9775ea7faf701918a58
Ajay Datla <ajaydatlalbn06@gmail.com>
2020-08-11 23:35:45 +0530

    Create Kadane.java
    Kadane's algorithm

---
+++ src/main/java/com/gfg/Kadane.java
@@ -0,0 +1,32 @@
+package com.gfg;
+
+import java.io.*; 
+// Java program to print largest contiguous array sum 
+import java.util.*; 
+  
+class Kadane 
+{ 
+    public static void main (String[] args) 
+    { 
+        int [] a = {-2, -3, -4, -1, -2, -1, -5, -3}; 
+        System.out.println("Maximum contiguous sum is " + maxSubArraySum(a)); 
+//        System.out.println("Int min "+Integer.MIN_VALUE);
+//        System.out.println("Int max "+Integer.MAX_VALUE);
+    } 
+  
+    static int maxSubArraySum(int a[]) 
+    { 
+        int size = a.length; 
+        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0; 
+  
+        for (int i = 0; i < size; i++) 
+        { 
+            max_ending_here = max_ending_here + a[i]; 
+            if (max_so_far < max_ending_here) 
+                max_so_far = max_ending_here; 
+            if (max_ending_here < 0) 
+                max_ending_here = 0; 
+        } 
+        return max_so_far; 
+    } 
+} 
