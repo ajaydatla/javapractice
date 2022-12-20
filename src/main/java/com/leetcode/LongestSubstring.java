e66149373e3e08c99cc8f6117798dc65667f7dff
Ajay <ajaydatlalbn06@gmail.com>
2022-12-20 13:32:51 +0530

    Leetcode 1

---
+++ src/main/java/com/leetcode/LongestSubstring.java
@@ -0,0 +1,25 @@
+package com.leetcode;
+
+import java.util.HashMap;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String s = "tmmzuxt";
+        HashMap<Character, Integer> map = new HashMap<>();
+        Integer length = 0;
+        Integer longestLength = 0;
+        for(int i = 0; i< s.length(); i ++){
+            length++;
+            if(map.containsKey(s.charAt(i))){
+                int oldIndex = map.get(s.charAt(i));
+                map.replace(s.charAt(i), i);
+                length = length - oldIndex-1;
+            }else{
+                map.put(s.charAt(i), i);
+//                length++;
+            }
+            if(length > longestLength) longestLength = length;
+        }
+        System.out.println(longestLength);
+    }
+}
