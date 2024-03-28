96fde3f29572f65664932934dbac215bb9d052a8
ajaydatla <ajaydatlalbn06@gmail.com>
2024-03-28 22:46:23 +0530

    asterisk triangle

--- src/main/java/com/test/JavapracticeApplication.java
+++ src/main/java/com/test/JavapracticeApplication.java
@@ -1,12 +1,7 @@
 package com.test;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class JavapracticeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavapracticeApplication.class, args);
-	}
+
 }
